7917737127d180da16ccc18f3408262ca050916f
Just-Vlados <vlad.lapets@gmail.com>
2017-02-03 17:24:06 +0300

    guessing-game: add all methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.minValue = 0;
+    	this.maxValue = 0;
+    	this.currentNumber = 0;
+    }
 
     setRange(min, max) {
-
+    	this.minValue = min;
+    	this.maxValue = max;
     }
 
     guess() {
-
+    	this.currentNumber = Math.round((this.minValue + this.maxValue)/2);
+    	return this.currentNumber;
     }
 
     lower() {
-
+    	this.maxValue = this.currentNumber;
     }
 
     greater() {
-
+    	this.minValue = this.currentNumber;
     }
 }
 
